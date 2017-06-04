416dfa0ec23831255e884feef3e56ded7f03ac44
Antoine-E <antoine.esquirol@gmail.com>
2017-06-04 23:51:04 +0200

    commit

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
-	require "\conf.inc.php";
-	require "\lib.php";
-	include "admin\libSQL.php";
+	require "conf.inc.php";
+	require "lib.php";
+	include "admin/libSQL.php";
 
 
 
