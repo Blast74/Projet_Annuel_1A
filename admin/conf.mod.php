86dbacfb9ddd4dd7e07fd9620433101c8460de8e
Blast74 <venzo.terence@gmail.com>
2017-06-04 18:31:52 +0200

    conf admin

--- admin/conf.mod.php
+++ admin/conf.mod.php
@@ -1,8 +1,14 @@
 <?php
 
-$returnModCheck = [
-    "moderator" => [TRUE, "Moderator"],
-    "supermoderator" => [TRUE, "Supermoderator"],
-    "user" => [FALSE, "User"],
-    "error" => [FALSE, "Error"]
-];
+const CHECKMODERATOR = [1, "Moderator"];
+const CHECKSUPERMODERATOR = [1, "Supermoderator"];
+const CHECKUSER = [0, "User"];
+const CHECKERROR = [0, "Error"];
+
+
+
+  //     "moderator" => [TRUE, "Moderator"],
+  //     "supermoderator" => [TRUE, "Supermoderator"],
+  //     "user" => [FALSE, "User"],
+  //     "error" => [FALSE, "Error"]
+  // ];
